7c56232ebbe3fa7ffc199d0c1e763fd647df2b2b
nurujjamanpollob <nurujjamanpollob@gmail.com>
2025-07-17 12:43:03 +0600

    rename from generic "Main" Name to Specific "BulkStringReplacer" Name

--- run-module/src/main/java/javadev/stringcollections/textreplacor/BulkStringReplacer.java
+++ run-module/src/main/java/javadev/stringcollections/textreplacor/BulkStringReplacer.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class BulkStringReplacer {
     public static void main(String[] args) {
         System.out.println("Welcome to bulk String replacement tool.");
         System.out.println("This tool will help you to replace a text in all files under a directory with another text.");
